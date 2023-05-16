17ddcc7b67b411f3f8f7fa891c4b96631e2a46ce
saffronjam <own.emka@gmail.com>
2023-05-16 21:48:29 +0200

    remove unused field in user update struct

--- models/sys/user/models.go
+++ models/sys/user/models.go
@@ -23,7 +23,5 @@ type User struct {
 type UserUpdate struct {
 	Username        *string      `json:"username" bson:"username"`
 	Email           *string      `json:"email" bson:"email"`
-	VmQuota         *int         `json:"vmQuota" bson:"vmQuota"`
-	DeploymentQuota *int         `json:"deploymentQuota" bson:"deploymentQuota"`
 	PublicKeys      *[]PublicKey `json:"publicKeys" bson:"publicKeys"`
 }
