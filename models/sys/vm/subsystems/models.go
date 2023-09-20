deb3a5712b086a3279565cf0d466d59176224d38
saffronjam <own.emka@gmail.com>
2023-09-20 11:37:52 +0200

    bug fix maps needs to have omitEmpty to not push nil maps to db

--- models/sys/vm/subsystems/models.go
+++ models/sys/vm/subsystems/models.go
@@ -5,6 +5,6 @@ import csModels "go-deploy/pkg/subsystems/cs/models"
 type CS struct {
 	ServiceOffering       csModels.ServiceOfferingPublic               `bson:"serviceOffering"`
 	VM                    csModels.VmPublic                            `bson:"vm"`
-	PortForwardingRuleMap map[string]csModels.PortForwardingRulePublic `bson:"portForwardingRuleMap"`
-	SnapshotMap           map[string]csModels.SnapshotPublic           `bson:"snapshotMap"`
+	PortForwardingRuleMap map[string]csModels.PortForwardingRulePublic `bson:"portForwardingRuleMap,omitempty"`
+	SnapshotMap           map[string]csModels.SnapshotPublic           `bson:"snapshotMap,omitempty"`
 }
