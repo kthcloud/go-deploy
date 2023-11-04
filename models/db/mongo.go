9ae6d7a83ff6d91c1235ff8acba8b1949094e301
saffronjam <own.emka@gmail.com>
2023-11-04 13:27:29 +0100

    allow creating mongdb partial unique indexes

--- models/db/mongo.go
+++ models/db/mongo.go
@@ -12,10 +12,13 @@ import (
 )
 
 type CollectionDefinition struct {
-	Name            string
-	Indexes         []string
-	UniqueIndexes   []string
-	TextIndexFields []string
+	Name    string
+	Indexes []string
+	// unique only for non-deleted documents
+	UniqueIndexes []string
+	// unique even for deleted documents
+	TotallyUniqueIndexes []string
+	TextIndexFields      []string
 }
 
 func (dbCtx *Context) setupMongo() error {
@@ -78,7 +81,7 @@ func (dbCtx *Context) setupMongo() error {
 		for _, indexName := range def.UniqueIndexes {
 			_, err = DB.GetCollection(def.Name).Indexes().CreateOne(context.Background(), mongo.IndexModel{
 				Keys:    map[string]int{indexName: 1},
-				Options: options.Index().SetUnique(true),
+				Options: options.Index().SetUnique(true).SetPartialFilterExpression(bson.D{{"deletedAt", bson.D{{"$in", []interface{}{nil, time.Time{}}}}}}),
 			})
 			if err != nil {
 				return makeError(err)
@@ -88,6 +91,22 @@ func (dbCtx *Context) setupMongo() error {
 	}
 
 	log.Println("ensured", createdCount, "unique indexes")
+
+	createdCount = 0
+	for _, def := range DB.CollectionDefinitionMap {
+		for _, indexName := range def.UniqueIndexes {
+			_, err = DB.GetCollection(def.Name).Indexes().CreateOne(context.Background(), mongo.IndexModel{
+				Keys:    map[string]int{indexName: 1},
+				Options: options.Index().SetUnique(true),
+			})
+			if err != nil {
+				return makeError(err)
+			}
+			createdCount++
+		}
+	}
+
+	log.Println("ensured", createdCount, "totally unique indexes")
 
 	createdCount = 0
 	for _, def := range DB.CollectionDefinitionMap {
@@ -132,51 +151,53 @@ func (dbCtx *Context) shutdownMongo() error {
 func getCollectionDefinitions() map[string]CollectionDefinition {
 	return map[string]CollectionDefinition{
 		"deployments": {
-			Name:          "deployments",
-			Indexes:       []string{"name", "ownerId", "type", "statusCode", "createdAt", "deletedAt", "repairedAt", "restartedAt", "zone"},
-			UniqueIndexes: []string{"id"},
+			Name:                 "deployments",
+			Indexes:              []string{"ownerId", "type", "statusCode", "createdAt", "deletedAt", "repairedAt", "restartedAt", "zone"},
+			UniqueIndexes:        []string{"name"},
+			TotallyUniqueIndexes: []string{"id"},
 		},
 		"storageManagers": {
-			Name:          "storageManagers",
-			Indexes:       []string{"name", "ownerId", "createdAt", "deletedAt", "repairedAt", "zone"},
-			UniqueIndexes: []string{"id"},
+			Name:                 "storageManagers",
+			Indexes:              []string{"ownerId", "createdAt", "deletedAt", "repairedAt", "zone"},
+			TotallyUniqueIndexes: []string{"id"},
 		},
 		"vms": {
-			Name:          "vms",
-			Indexes:       []string{"name", "ownerId", "gpuId", "statusCode", "createdAt", "deletedAt", "repairedAt", "restartedAt", "zone"},
-			UniqueIndexes: []string{"id"},
+			Name:                 "vms",
+			Indexes:              []string{"ownerId", "gpuId", "statusCode", "createdAt", "deletedAt", "repairedAt", "restartedAt", "zone"},
+			UniqueIndexes:        []string{"name"},
+			TotallyUniqueIndexes: []string{"id"},
 		},
 		"gpus": {
-			Name:          "gpus",
-			Indexes:       []string{"name", "host", "lease.vmId", "lease.user", "lease.end"},
-			UniqueIndexes: []string{"id"},
+			Name:                 "gpus",
+			Indexes:              []string{"name", "host", "lease.vmId", "lease.user", "lease.end"},
+			TotallyUniqueIndexes: []string{"id"},
 		},
 		"users": {
-			Name:            "users",
-			Indexes:         []string{"username", "email", "firstName", "lastName", "effectiveRole.name"},
-			UniqueIndexes:   []string{"id"},
-			TextIndexFields: []string{"username", "email", "firstName", "lastName"},
+			Name:                 "users",
+			Indexes:              []string{"username", "email", "firstName", "lastName", "effectiveRole.name"},
+			TotallyUniqueIndexes: []string{"id"},
+			TextIndexFields:      []string{"username", "email", "firstName", "lastName"},
 		},
 		"teams": {
-			Name:            "teams",
-			Indexes:         []string{"name", "ownerId", "createdAt", "deletedAt"},
-			UniqueIndexes:   []string{"id"},
-			TextIndexFields: []string{"name"},
+			Name:                 "teams",
+			Indexes:              []string{"name", "ownerId", "createdAt", "deletedAt"},
+			TotallyUniqueIndexes: []string{"id"},
+			TextIndexFields:      []string{"name"},
 		},
 		"jobs": {
-			Name:          "jobs",
-			Indexes:       []string{"userId", "type", "args.id", "status", "createdAt", "runAfter"},
-			UniqueIndexes: []string{"id"},
+			Name:                 "jobs",
+			Indexes:              []string{"userId", "type", "args.id", "status", "createdAt", "runAfter"},
+			TotallyUniqueIndexes: []string{"id"},
 		},
 		"notifications": {
-			Name:          "notifications",
-			Indexes:       []string{"userId", "type", "createdAt", "readAt", "deletedAt"},
-			UniqueIndexes: []string{"id"},
+			Name:                 "notifications",
+			Indexes:              []string{"userId", "type", "createdAt", "readAt", "deletedAt"},
+			TotallyUniqueIndexes: []string{"id"},
 		},
 		"events": {
-			Name:          "events",
-			Indexes:       []string{"type", "createdAt", "source.userId"},
-			UniqueIndexes: []string{"id"},
+			Name:                 "events",
+			Indexes:              []string{"type", "createdAt", "source.userId"},
+			TotallyUniqueIndexes: []string{"id"},
 		},
 	}
 }
