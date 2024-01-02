9d475131baf561b002c3320acef3d1097ebefc25
saffronjam <own.emka@gmail.com>
2024-01-02 01:59:22 +0100

    remove old migration

--- pkg/workers/migrate/migrate.go
+++ pkg/workers/migrate/migrate.go
@@ -1,9 +1,6 @@
 package migrator
 
 import (
-	"errors"
-	vmModels "go-deploy/models/sys/vm"
-	vmPortModels "go-deploy/models/sys/vm_port"
 	"log"
 )
 
@@ -36,37 +33,5 @@ func Migrate() {
 //
 // add a date to the migration name to make it easier to identify.
 func getMigrations() map[string]func() error {
-	return map[string]func() error{
-		"leaseVmPortsFromOldSystem_2023_10_30": leaseVmPortsFromOldSystem,
-	}
-}
-
-func leaseVmPortsFromOldSystem() error {
-	vms, err := vmModels.New().List()
-	if err != nil {
-		return err
-	}
-
-	for _, vm := range vms {
-		for _, pfr := range vm.Subsystems.CS.PortForwardingRuleMap {
-			vmPort, err := vmPortModels.New().GetByLease(vm.ID, pfr.PrivatePort)
-			if err != nil {
-				return err
-			}
-
-			if vmPort == nil {
-
-				_, err = vmPortModels.New().Lease(pfr.PublicPort, pfr.PrivatePort, vm.ID, vm.Zone)
-				if err != nil {
-					if errors.Is(err, vmPortModels.PortNotFoundErr) {
-						return err
-					}
-
-					return err
-				}
-			}
-		}
-	}
-
-	return nil
+	return map[string]func() error{}
 }
