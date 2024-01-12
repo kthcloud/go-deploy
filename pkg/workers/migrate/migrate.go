284822a1bba789b13b02f2e6e6c5d01be8509799
saffronjam <own.emka@gmail.com>
2024-01-12 15:06:54 +0100

    fix migration scripts for new custom domain and port map system

--- pkg/workers/migrate/migrate.go
+++ pkg/workers/migrate/migrate.go
@@ -1,9 +1,13 @@
 package migrator
 
 import (
+	"go-deploy/models/sys/base/resource"
 	deploymentModels "go-deploy/models/sys/deployment"
+	vmModels "go-deploy/models/sys/vm"
 	"go.mongodb.org/mongo-driver/bson"
 	"log"
+	"strconv"
+	"strings"
 )
 
 // Migrate will run as early as possible in the program, and it will never be called again.
@@ -36,7 +40,10 @@ func Migrate() {
 // add a date to the migration name to make it easier to identify.
 func getMigrations() map[string]func() error {
 	return map[string]func() error{
-		"migrateCustomDomainStatusReadyToActive_2024_01_12": migrateCustomDomainStatusReadyToActive_2024_01_12,
+		"migrateCustomDomainStatusReadyToActive_2024_01_12":               migrateCustomDomainStatusReadyToActive_2024_01_12,
+		"migrateOldCustomDomainToNewStruct_2024_01_02":                    migrateOldCustomDomainToNewStruct_2024_01_02,
+		"migrateAwayFromNullFields_VmHttpProxyAndCustomDomain_2024_01_12": migrateAwayFromNullFields_VmHttpProxyAndCustomDomain_2024_01_12,
+		"migratePortListToPortMap_2024_01_12":                             migratePortListToPortMap_2024_01_12,
 	}
 }
 
@@ -55,6 +62,121 @@ func migrateCustomDomainStatusReadyToActive_2024_01_12() error {
 
 			if err != nil {
 				return err
+			}
+		}
+	}
+
+	return nil
+}
+
+type oldVM1 struct {
+	ID    string `bson:"id"`
+	Ports []struct {
+		Name      string `bson:"name"`
+		HttpProxy *struct {
+			CustomDomain *string `bson:"customDomain"`
+		}
+	}
+}
+
+func migrateOldCustomDomainToNewStruct_2024_01_02() error {
+	rc := resource.ResourceClient[oldVM1]{
+		Collection: vmModels.New().Collection,
+	}
+
+	ids, err := vmModels.New().ListIDs()
+	if err != nil {
+		return err
+	}
+
+	for _, id := range ids {
+		vm, err := rc.GetByID(id.ID)
+		if err != nil {
+			if strings.Contains(err.Error(), "error decoding key") {
+				continue
+			}
+
+			return err
+		}
+
+		for idx, port := range vm.Ports {
+			if port.HttpProxy != nil && port.HttpProxy.CustomDomain != nil {
+				customDomain := vmModels.CustomDomain{
+					Domain: *port.HttpProxy.CustomDomain,
+					Secret: "",
+					Status: deploymentModels.CustomDomainStatusActive,
+				}
+
+				updatePath := "ports." + strconv.Itoa(idx) + ".httpProxy.customDomain"
+
+				err = rc.SetWithBsonByID(id.ID, bson.D{
+					{updatePath, customDomain},
+				})
+
+				if err != nil {
+					return err
+				}
+			}
+		}
+	}
+
+	return nil
+}
+
+func migratePortListToPortMap_2024_01_12() error {
+	vms, err := vmModels.New().List()
+	if err != nil {
+		return err
+	}
+
+	for _, vm := range vms {
+		// ssh will always be there, so this check is safe
+		if len(vm.PortMap) > 0 {
+			continue
+		}
+
+		portMap := make(map[string]vmModels.Port)
+
+		for _, port := range vm.Ports_ {
+			portMap[port.Name] = port
+		}
+
+		err = vmModels.New().SetWithBsonByID(vm.ID, bson.D{
+			{"portMap", portMap},
+		})
+
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func migrateAwayFromNullFields_VmHttpProxyAndCustomDomain_2024_01_12() error {
+	vms, err := vmModels.New().List()
+	if err != nil {
+		return err
+	}
+
+	for _, vm := range vms {
+		for mapName, port := range vm.PortMap {
+			if port.HttpProxy == nil {
+				err = vmModels.New().UnsetWithBsonByID(vm.ID, "portMap."+mapName+".httpProxy")
+				if err != nil {
+					return err
+				}
+
+				continue
+			}
+
+			if port.HttpProxy.CustomDomain == nil {
+				err = vmModels.New().UnsetWithBsonByID(vm.ID, "portMap."+mapName+".httpProxy.customDomain")
+				if err != nil {
+					return err
+				}
+
+				continue
 			}
 		}
 	}
