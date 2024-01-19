5be93cd576b7ecd449195af70b90ab798638480b
saffronjam <own.emka@gmail.com>
2024-01-19 11:33:52 +0100

    fix broken acc tests

--- test/acc/subsystems/k8s/helpers.go
+++ test/acc/subsystems/k8s/helpers.go
@@ -40,7 +40,7 @@ func withNamespace(t *testing.T) *models.NamespacePublic {
 
 	nCreated, err := c.CreateNamespace(n)
 	test.NoError(t, err, "failed to create namespace")
-	assert.NotZero(t, nCreated.ID, "no namespace id received from client")
+	assert.True(t, nCreated.Created(), "namespace was not created")
 	t.Cleanup(func() { cleanUpNamespace(t, c, nCreated.Name) })
 
 	assert.Equal(t, n.Name, nCreated.Name, "namespace name does not match")
@@ -62,8 +62,8 @@ func withDefaultDeployment(t *testing.T, c *k8s.Client) *models.DeploymentPublic
 func withDeployment(t *testing.T, c *k8s.Client, d *models.DeploymentPublic) *models.DeploymentPublic {
 	dCreated, err := c.CreateDeployment(d)
 	test.NoError(t, err, "failed to create deployment")
-	assert.NotZero(t, dCreated.ID, "no deployment id received from client")
-	t.Cleanup(func() { cleanUpDeployment(t, c, dCreated.ID) })
+	assert.True(t, dCreated.Created(), "deployment was not created")
+	t.Cleanup(func() { cleanUpDeployment(t, c, dCreated.Name) })
 
 	assert.Equal(t, d.Name, dCreated.Name, "deployment name does not match")
 	assert.Equal(t, d.Namespace, dCreated.Namespace, "deployment namespace does not match")
@@ -87,8 +87,8 @@ func withDefaultService(t *testing.T, c *k8s.Client) *models.ServicePublic {
 func withService(t *testing.T, c *k8s.Client, s *models.ServicePublic) *models.ServicePublic {
 	sCreated, err := c.CreateService(s)
 	test.NoError(t, err, "failed to create service")
-	assert.NotZero(t, sCreated.ID, "no service id received from client")
-	t.Cleanup(func() { cleanUpService(t, c, sCreated.ID) })
+	assert.True(t, sCreated.Created(), "service was not created")
+	t.Cleanup(func() { cleanUpService(t, c, sCreated.Name) })
 
 	assert.Equal(t, s.Name, sCreated.Name, "service name does not match")
 	assert.Equal(t, s.Namespace, sCreated.Namespace, "service namespace does not match")
@@ -114,8 +114,8 @@ func withDefaultIngress(t *testing.T, c *k8s.Client, s *models.ServicePublic) *m
 func withIngress(t *testing.T, c *k8s.Client, i *models.IngressPublic) *models.IngressPublic {
 	iCreated, err := c.CreateIngress(i)
 	test.NoError(t, err, "failed to create ingress")
-	assert.NotZero(t, iCreated.ID, "no ingress id received from client")
-	t.Cleanup(func() { cleanUpIngress(t, c, iCreated.ID) })
+	assert.True(t, iCreated.Created(), "ingress was not created")
+	t.Cleanup(func() { cleanUpIngress(t, c, iCreated.Name) })
 
 	assert.Equal(t, i.Name, iCreated.Name, "ingress name does not match")
 	assert.Equal(t, i.Namespace, iCreated.Namespace, "ingress namespace does not match")
@@ -141,8 +141,8 @@ func withDefaultPVC(t *testing.T, c *k8s.Client, pv *models.PvPublic) *models.Pv
 func withPVC(t *testing.T, c *k8s.Client, pvc *models.PvcPublic) *models.PvcPublic {
 	pvcCreated, err := c.CreatePVC(pvc)
 	test.NoError(t, err, "failed to create pvc")
-	assert.NotZero(t, pvcCreated.ID, "no pvc id received from client")
-	t.Cleanup(func() { cleanUpPVC(t, c, pvcCreated.ID) })
+	assert.True(t, pvcCreated.Created(), "pvc was not created")
+	t.Cleanup(func() { cleanUpPVC(t, c, pvcCreated.Name) })
 
 	assert.Equal(t, pvc.Name, pvcCreated.Name, "pvc name does not match")
 	assert.Equal(t, pvc.Namespace, pvcCreated.Namespace, "pvc namespace does not match")
@@ -165,8 +165,8 @@ func withDefaultPV(t *testing.T, c *k8s.Client) *models.PvPublic {
 func withPV(t *testing.T, c *k8s.Client, pv *models.PvPublic) *models.PvPublic {
 	pvCreated, err := c.CreatePV(pv)
 	test.NoError(t, err, "failed to create pv")
-	assert.NotZero(t, pvCreated.ID, "no pv id received from client")
-	t.Cleanup(func() { cleanUpPV(t, c, pvCreated.ID) })
+	assert.True(t, pvCreated.Created(), "pv was not created")
+	t.Cleanup(func() { cleanUpPV(t, c, pvCreated.Name) })
 
 	assert.Equal(t, pv.Name, pvCreated.Name, "pv name does not match")
 	assert.Equal(t, pv.Capacity, pvCreated.Capacity, "pv capacity does not match")
@@ -197,8 +197,8 @@ func withDefaultHPA(t *testing.T, c *k8s.Client, d *models.DeploymentPublic) *mo
 func withHPA(t *testing.T, c *k8s.Client, hpa *models.HpaPublic) *models.HpaPublic {
 	hpaCreated, err := c.CreateHPA(hpa)
 	test.NoError(t, err, "failed to create hpa")
-	assert.NotZero(t, hpaCreated.ID, "no hpa id received from client")
-	t.Cleanup(func() { cleanUpHPA(t, c, hpaCreated.ID) })
+	assert.True(t, hpaCreated.Created(), "hpa was not created")
+	t.Cleanup(func() { cleanUpHPA(t, c, hpaCreated.Name) })
 
 	assert.Equal(t, hpa.Name, hpaCreated.Name, "hpa name does not match")
 	assert.Equal(t, hpa.Namespace, hpaCreated.Namespace, "hpa namespace does not match")
@@ -225,8 +225,8 @@ func withDefaultSecret(t *testing.T, c *k8s.Client) *models.SecretPublic {
 func withSecret(t *testing.T, c *k8s.Client, secret *models.SecretPublic) *models.SecretPublic {
 	secretCreated, err := c.CreateSecret(secret)
 	test.NoError(t, err, "failed to create secret")
-	assert.NotZero(t, secretCreated.ID, "no secret id received from client")
-	t.Cleanup(func() { cleanUpSecret(t, c, secretCreated.ID) })
+	assert.True(t, secretCreated.Created(), "secret was not created")
+	t.Cleanup(func() { cleanUpSecret(t, c, secretCreated.Name) })
 
 	assert.Equal(t, secret.Name, secretCreated.Name, "secret name does not match")
 	assert.Equal(t, secret.Namespace, secretCreated.Namespace, "secret namespace does not match")
@@ -252,8 +252,8 @@ func withDefaultJob(t *testing.T, c *k8s.Client) *models.JobPublic {
 func withJob(t *testing.T, c *k8s.Client, job *models.JobPublic) *models.JobPublic {
 	jobCreated, err := c.CreateJob(job)
 	test.NoError(t, err, "failed to create job")
-	assert.NotZero(t, jobCreated.ID, "no job id received from client")
-	t.Cleanup(func() { cleanUpJob(t, c, jobCreated.ID) })
+	assert.True(t, jobCreated.Created(), "job was not created")
+	t.Cleanup(func() { cleanUpJob(t, c, jobCreated.Name) })
 
 	assert.Equal(t, job.Name, jobCreated.Name, "job name does not match")
 	assert.Equal(t, job.Namespace, jobCreated.Namespace, "job namespace does not match")
@@ -276,98 +276,98 @@ func cleanUpNamespace(t *testing.T, c *k8s.Client, name string) {
 	test.NoError(t, err, "failed to delete namespace again")
 }
 
-func cleanUpDeployment(t *testing.T, c *k8s.Client, id string) {
-	err := c.DeleteDeployment(id)
+func cleanUpDeployment(t *testing.T, c *k8s.Client, name string) {
+	err := c.DeleteDeployment(name)
 	test.NoError(t, err, "failed to delete deployment")
 
-	deletedDeployment, err := c.ReadDeployment(id)
+	deletedDeployment, err := c.ReadDeployment(name)
 	test.NoError(t, err, "failed to read deployment")
 	assert.Nil(t, deletedDeployment, "deployment still exists")
 
-	err = c.DeleteDeployment(id)
+	err = c.DeleteDeployment(name)
 	test.NoError(t, err, "failed to delete deployment again")
 }
 
-func cleanUpService(t *testing.T, c *k8s.Client, id string) {
-	err := c.DeleteService(id)
+func cleanUpService(t *testing.T, c *k8s.Client, name string) {
+	err := c.DeleteService(name)
 	test.NoError(t, err, "failed to delete service")
 
-	deletedService, err := c.ReadService(id)
+	deletedService, err := c.ReadService(name)
 	test.NoError(t, err, "failed to read service")
 	assert.Nil(t, deletedService, "service still exists")
 
-	err = c.DeleteService(id)
+	err = c.DeleteService(name)
 	test.NoError(t, err, "failed to delete service again")
 }
 
-func cleanUpIngress(t *testing.T, c *k8s.Client, id string) {
-	err := c.DeleteIngress(id)
+func cleanUpIngress(t *testing.T, c *k8s.Client, name string) {
+	err := c.DeleteIngress(name)
 	test.NoError(t, err, "failed to delete ingress")
 
-	deletedIngress, err := c.ReadIngress(id)
+	deletedIngress, err := c.ReadIngress(name)
 	test.NoError(t, err, "failed to read ingress")
 	assert.Nil(t, deletedIngress, "ingress still exists")
 
-	err = c.DeleteIngress(id)
+	err = c.DeleteIngress(name)
 	test.NoError(t, err, "failed to delete ingress again")
 }
 
-func cleanUpPVC(t *testing.T, c *k8s.Client, id string) {
-	err := c.DeletePVC(id)
+func cleanUpPVC(t *testing.T, c *k8s.Client, name string) {
+	err := c.DeletePVC(name)
 	test.NoError(t, err, "failed to delete pvc")
 
-	deletedPVC, err := c.ReadPVC(id)
+	deletedPVC, err := c.ReadPVC(name)
 	test.NoError(t, err, "failed to read pvc")
 	assert.Nil(t, deletedPVC, "pvc still exists")
 
-	err = c.DeletePVC(id)
+	err = c.DeletePVC(name)
 	test.NoError(t, err, "failed to delete pvc again")
 }
 
-func cleanUpPV(t *testing.T, c *k8s.Client, id string) {
-	err := c.DeletePV(id)
+func cleanUpPV(t *testing.T, c *k8s.Client, name string) {
+	err := c.DeletePV(name)
 	test.NoError(t, err, "failed to delete pv")
 
-	deletedPV, err := c.ReadPV(id)
+	deletedPV, err := c.ReadPV(name)
 	test.NoError(t, err, "failed to read pv")
 	assert.Nil(t, deletedPV, "pv still exists")
 
-	err = c.DeletePV(id)
+	err = c.DeletePV(name)
 	test.NoError(t, err, "failed to delete pv again")
 }
 
-func cleanUpHPA(t *testing.T, c *k8s.Client, id string) {
-	err := c.DeleteHPA(id)
+func cleanUpHPA(t *testing.T, c *k8s.Client, name string) {
+	err := c.DeleteHPA(name)
 	test.NoError(t, err, "failed to delete hpa")
 
-	deletedHPA, err := c.ReadHPA(id)
+	deletedHPA, err := c.ReadHPA(name)
 	test.NoError(t, err, "failed to read hpa")
 	assert.Nil(t, deletedHPA, "hpa still exists")
 
-	err = c.DeleteHPA(id)
+	err = c.DeleteHPA(name)
 	test.NoError(t, err, "failed to delete hpa again")
 }
 
-func cleanUpSecret(t *testing.T, c *k8s.Client, id string) {
-	err := c.DeleteSecret(id)
+func cleanUpSecret(t *testing.T, c *k8s.Client, name string) {
+	err := c.DeleteSecret(name)
 	test.NoError(t, err, "failed to delete secret")
 
-	deletedSecret, err := c.ReadSecret(id)
+	deletedSecret, err := c.ReadSecret(name)
 	test.NoError(t, err, "failed to read secret")
 	assert.Nil(t, deletedSecret, "secret still exists")
 
-	err = c.DeleteSecret(id)
+	err = c.DeleteSecret(name)
 	test.NoError(t, err, "failed to delete secret again")
 }
 
-func cleanUpJob(t *testing.T, c *k8s.Client, id string) {
-	err := c.DeleteJob(id)
+func cleanUpJob(t *testing.T, c *k8s.Client, name string) {
+	err := c.DeleteJob(name)
 	test.NoError(t, err, "failed to delete job")
 
-	deletedJob, err := c.ReadJob(id)
+	deletedJob, err := c.ReadJob(name)
 	test.NoError(t, err, "failed to read job")
 	assert.Nil(t, deletedJob, "job still exists")
 
-	err = c.DeleteJob(id)
+	err = c.DeleteJob(name)
 	test.NoError(t, err, "failed to delete job again")
 }
