b2bd67807390d3e219b1f25595fd193689d9ddbd
saffronjam <own.emka@gmail.com>
2024-02-09 14:22:38 +0100

    add tests for user data

---
+++ test/e2e/user_data/routes_test.go
@@ -0,0 +1,67 @@
+package user_data
+
+import (
+	"github.com/stretchr/testify/assert"
+	"go-deploy/models/dto/v1/body"
+	"go-deploy/test/e2e"
+	"os"
+	"testing"
+)
+
+func TestMain(m *testing.M) {
+	e2e.Setup()
+	code := m.Run()
+	e2e.Shutdown()
+	os.Exit(code)
+}
+
+func TestGet(t *testing.T) {
+	t.Parallel()
+
+	userData := e2e.WithUserData(t, body.UserDataCreate{
+		ID:   e2e.GenName("userData"),
+		Data: "test",
+	})
+
+	userDataRead := e2e.GetUserData(t, userData.ID)
+	if userDataRead.ID != userData.ID {
+		t.Error("user data was not fetched")
+	}
+}
+
+func TestList(t *testing.T) {
+	t.Parallel()
+
+	userData := e2e.WithUserData(t, body.UserDataCreate{
+		ID:   e2e.GenName("userData"),
+		Data: "test",
+	})
+
+	userDataList := e2e.ListUserData(t, "")
+	assert.NotEmpty(t, userDataList, "user data was not fetched")
+
+	found := false
+	for _, ud := range userDataList {
+		if ud.ID == userData.ID {
+			found = true
+			break
+		}
+	}
+
+	assert.True(t, found, "user data was not fetched")
+}
+
+func TestUpdate(t *testing.T) {
+	t.Parallel()
+
+	userData := e2e.WithUserData(t, body.UserDataCreate{
+		ID:   e2e.GenName("userData"),
+		Data: "test",
+	})
+
+	userDataUpdate := e2e.UpdateUserData(t, userData.ID, body.UserDataUpdate{
+		Data: "test2",
+	})
+
+	assert.Equal(t, "test2", userDataUpdate.Data, "user data was not updated")
+}
---
+++ test/e2e/user_data_utils.go
@@ -0,0 +1,59 @@
+package e2e
+
+import (
+	"github.com/stretchr/testify/assert"
+	"go-deploy/models/dto/v1/body"
+	"net/http"
+	"testing"
+)
+
+func GetUserData(t *testing.T, id string, userID ...string) body.UserDataRead {
+	resp := DoGetRequest(t, "/userData/"+id, userID...)
+	assert.Equal(t, http.StatusOK, resp.StatusCode, "user data was not fetched")
+
+	var userDataRead body.UserDataRead
+	err := ReadResponseBody(t, resp, &userDataRead)
+	assert.NoError(t, err, "user data was not fetched")
+
+	return userDataRead
+}
+
+func ListUserData(t *testing.T, query string, userID ...string) []body.UserDataRead {
+	resp := DoGetRequest(t, "/userData"+query, userID...)
+	assert.Equal(t, http.StatusOK, resp.StatusCode, "user data was not fetched")
+
+	var userData []body.UserDataRead
+	err := ReadResponseBody(t, resp, &userData)
+	assert.NoError(t, err, "user data was not fetched")
+
+	return userData
+}
+
+func UpdateUserData(t *testing.T, id string, userDataUpdate body.UserDataUpdate, userID ...string) body.UserDataRead {
+	resp := DoPostRequest(t, "/userData/"+id, userDataUpdate, userID...)
+	var userDataRead body.UserDataRead
+	err := ReadResponseBody(t, resp, &userDataRead)
+	assert.NoError(t, err, "user data was not updated")
+
+	assert.Equal(t, userDataUpdate.Data, userDataRead.Data, "invalid user data name")
+
+	return userDataRead
+}
+
+func DeleteUserData(t *testing.T, id string, userID ...string) {
+	resp := DoDeleteRequest(t, "/userData/"+id, userID...)
+	assert.Equal(t, http.StatusNoContent, resp.StatusCode, "user data was not deleted")
+}
+
+func WithUserData(t *testing.T, userDataCreate body.UserDataCreate, userID ...string) body.UserDataRead {
+	resp := DoPostRequest(t, "/userData", userDataCreate, userID...)
+	userData := Parse[body.UserDataRead](t, resp)
+
+	assert.Equal(t, userDataCreate.Data, userData.Data, "invalid user data name")
+
+	t.Cleanup(func() {
+		DeleteUserData(t, userData.ID, userID...)
+	})
+
+	return userData
+}
