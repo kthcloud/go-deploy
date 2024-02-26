2bf8ada2131d2bd4321641450e548ef04d8a347f
saffronjam <own.emka@gmail.com>
2024-02-26 15:48:40 +0100

    fix sm url not in SmRead

--- models/sys/sm/dto.go
+++ models/sys/sm/dto.go
@@ -1,25 +1,16 @@
 package sm
 
 import (
-	"fmt"
 	"go-deploy/models/dto/v1/body"
-	"go-deploy/service/constants"
 )
 
 // ToDTO converts an SM to a body.SmRead DTO.
 func (sm *SM) ToDTO() body.SmRead {
-	var url *string
-	ingress, ok := sm.Subsystems.K8s.IngressMap[constants.SmAppName]
-	if ok && ingress.Created() && len(ingress.Hosts) > 0 && len(ingress.Hosts[0]) > 0 {
-		fullURL := fmt.Sprintf("https://%s", ingress.Hosts[0])
-		url = &fullURL
-	}
-
 	return body.SmRead{
 		ID:        sm.ID,
 		OwnerID:   sm.OwnerID,
 		CreatedAt: sm.CreatedAt,
 		Zone:      sm.Zone,
-		URL:       url,
+		URL:       sm.GetURL(),
 	}
 }
