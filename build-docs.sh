71534a9764a840dd0aa01f9d9a9ddbb5a1f53a53
Emil Karlsson <own.emka@gmail.com>
2024-04-25 17:10:54 +0200

    Fix bad API docs

--- build-docs.sh
+++ build-docs.sh
@@ -1,3 +1,10 @@
-export PATH=$(go env GOPATH)/bin:$PATH
+echo "Installing swaggo/swag"
+go install github.com/swaggo/swag/cmd/swag@latest
+
+# You might need to "source ./scripts/path-cmd.sh"
+
+echo "Generating docs"
 swag init --dir ./  -o ./docs/api/v1 --exclude ./routers/api/v2 --instanceName "V1"
 swag init --dir ./  -o ./docs/api/v2 --exclude ./routers/api/v1 --instanceName "V2"
+
+echo "Completed"
