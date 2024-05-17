8d65eee437ce3bab6bbbe411fa996e4f177a595f
saffronjam <own.emka@gmail.com>
2024-05-17 20:03:17 +0200

    Remove old migration

--- pkg/db/migrate/migrate.go
+++ pkg/db/migrate/migrate.go
@@ -2,10 +2,7 @@ package migrator
 
 import (
 	"fmt"
-	"go-deploy/pkg/config"
-	"go-deploy/pkg/db/resources/deployment_repo"
 	"go-deploy/pkg/log"
-	"go.mongodb.org/mongo-driver/bson"
 )
 
 // Migrate runs every migration specified in the getMigrations function.
@@ -35,38 +32,5 @@ func Migrate() error {
 //
 // add a date to the migration name to make it easier to identify.
 func getMigrations() map[string]func() error {
-	return map[string]func() error{
-		"addSpecsToDeploymentWithoutSpecs_2024_05_10": addSpecsToDeploymentWithoutSpecs_2024_05_10,
-	}
-}
-
-func addSpecsToDeploymentWithoutSpecs_2024_05_10() error {
-	deployments, err := deployment_repo.New().List()
-	if err != nil {
-		return err
-	}
-
-	for _, deployment := range deployments {
-		for _, app := range deployment.Apps {
-			anyUpdated := false
-
-			if app.CpuCores == 0 {
-				app.CpuCores = config.Config.Deployment.Resources.Limits.CPU
-				anyUpdated = true
-			}
-			if app.RAM == 0 {
-				app.RAM = config.Config.Deployment.Resources.Limits.RAM
-				anyUpdated = true
-			}
-
-			if anyUpdated {
-				err = deployment_repo.New().SetWithBsonByID(deployment.ID, bson.D{{"apps." + app.Name, app}})
-				if err != nil {
-					return err
-				}
-			}
-		}
-	}
-
-	return nil
+	return map[string]func() error{}
 }
