9534c290183b512646bf1d997688ef7c0de902c3
kthcloud <noreply@cloud.cbh.kth.se>
2024-06-02 09:51:01 +0000

    [🚚 CI] Generate build from main

--- export/index.ts
+++ export/index.ts
@@ -2,14 +2,9 @@
  * Types for kthcloud go-deploy API
  *  */
 
-// v1 (CloudStack)
-import * as v1Body from "./types/v1/body";
-import * as v1Query from "./types/v1/query";
-import * as v1Uri from "./types/v1/uri";
-
 // v2  (Kubevirt)
 import * as v2Body from "./types/v2/body";
 import * as v2Query from "./types/v2/query";
 import * as v2Uri from "./types/v2/uri";
 
-export { v1Body, v1Query, v1Uri, v2Body, v2Query, v2Uri };
+export { v2Body, v2Query, v2Uri };
