f607d69304f13b61dffcfc7b8c9e4477130c36e3
Philip Zingmark <philip.zingmark@gmail.com>
2025-01-20 14:32:31 +0100

    add caching header to the docs files

--- docs/api/v2/handlers/serve_fs.go
+++ docs/api/v2/handlers/serve_fs.go
@@ -11,6 +11,9 @@ import (
 	swaggerfiles "github.com/swaggo/files/v2"
 )
 
+// Returns a handler that serves the requested any file
+// from swaggo/files/v2 embed FS with swagger UI and
+// the generated doc.json (V2_swagger.json)
 func ServeDocs(basePath string) func(ctx *gin.Context) {
 	return func(ctx *gin.Context) {
 		filePath := ctx.Param("any")
@@ -31,22 +34,24 @@ func ServeDocs(basePath string) func(ctx *gin.Context) {
 	layout: "StandaloneLayout"
   });
 };`, basePath)
+			ctx.Header("Cache-Control", "public, max-age=31536000")
 			ctx.Data(http.StatusOK, "application/javascript", []byte(customScript))
 			return
 		}
 
-		// Serve the Swagger documentation
 		if strings.TrimPrefix(filePath, "/") == "doc.json" {
 			doc, err := swag.ReadDoc("V2")
 			if err != nil {
 				ctx.String(http.StatusInternalServerError, "Failed to load documentation")
 				return
 			}
+			ctx.Header("Cache-Control", "public, max-age=3600")
 			ctx.Data(http.StatusOK, "application/json", []byte(doc))
 			return
 		}
 
 		ctx.Request.URL.Path = filePath
+		ctx.Header("Cache-Control", "public, max-age=31536000")
 		http.FileServer(http.FS(swaggerfiles.FS)).ServeHTTP(ctx.Writer, ctx.Request)
 	}
 }
